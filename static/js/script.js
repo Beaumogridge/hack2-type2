9ca06b0dc30c148f49da86207068f5c6ed2fa60c
Beaumogridge <92574836+Beaumogridge@users.noreply.github.com>
2021-10-28 14:04:52 +0000

    process made 28

---
+++ static/js/script.js
@@ -0,0 +1,47 @@
+console.log("Loaded script.js")
+
+function loadDirectory() {
+  console.log("Running loadDirectory()...");
+
+  var xhttp = new XMLHttpRequest();
+
+  xhttp.open("GET", "/Directory", true);
+  xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+  xhttp.onreadystatechange = function() {
+    console.log(xhttp.readyState);
+
+    if (xhttp.readyState === 4) {
+      console.log(xhttp.status);
+
+      if (xhttp.status === 200) {
+        var text = xhttp.responseText;
+        console.log(text);
+
+        var directory = JSON.parse(text);
+        console.log(directory);
+
+        document.getElementById("directoryDiv").innerHTML = text;
+        
+        for (var key in directory) {
+
+          var tr = document.createElement("tr");
+          var tdk = document.createElement("td");
+          var tdv = document.createElement("td");
+
+          tdk.appendChild(document.createTextNode(key));
+          tdv.appendChild(document.createTextNode(directory[key]));
+          tr.appendChild(tdk);
+          tr.appendChild(tdv);
+
+          document.getElementById('directoryTable').appendChild(tr);
+        }
+      } else {
+        console.log(xhttp.statusText);
+        console.error(xhttp.statusText);
+      }
+    }
+  };
+
+  xhttp.send();
+}
